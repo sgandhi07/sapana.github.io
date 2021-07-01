1addf818c5ad65ba28ff5dc41b8c8c3cddaef1df
sgandhi07 <julie@julies-MacBook-Air.local>
2021-07-01 07:56:50 -0400

    complete the first iteration of M&O pages and update css file to remove hover text decoration

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 
 document.getElementById("design").addEventListener("click", design);
 function design() {
+    document.getElementById("HLintro").style.display = "none";
   document.getElementById("containerOneImg").style.display = "block";
   document.getElementById("containerOneImg").src = "img/CAD.png";
   document.getElementById("figcaption1").innerHTML = "Above: Extract architectural details (benches) from CAD file using ArcMap."; 
@@ -16,6 +17,7 @@ function design() {
 
 document.getElementById("facilities").addEventListener("click", facilities);
 function facilities() {
+        document.getElementById("HLintro").style.display = "none";
   document.getElementById("containerOneImg").style.display = "block";
   document.getElementById("containerOneImg").src = "img/facilities1.png";
   document.getElementById("figcaption1").innerHTML = "Above: Basemap delineating operating zones for maitenance technicians."; 
@@ -31,6 +33,7 @@ function facilities() {
 
 document.getElementById("horticulture").addEventListener("click", horticulture);
 function horticulture() {
+        document.getElementById("HLintro").style.display = "none";
   document.getElementById("containerOneImg").style.display = "block";
   document.getElementById("containerOneImg").src = "img/hort1.png";
   document.getElementById("figcaption1").innerHTML = "Above: Basemap delineating horticultural zones for gardeners."; 
@@ -47,16 +50,17 @@ function horticulture() {
 
 document.getElementById("other").addEventListener("click", other);
 function other() {
+        document.getElementById("HLintro").style.display = "none";
   document.getElementById("containerOneImg").style.display = "block";
-  document.getElementById("containerOneImg").src = "img/hort1.png";
-  document.getElementById("figcaption1").innerHTML = "Above: Basemap delineating horticultural zones for gardeners."; 
+  document.getElementById("containerOneImg").src = "img/other4.png";
+  document.getElementById("figcaption1").innerHTML = ""; 
   document.getElementById("containerTwoImg").style.display = "block";
-  document.getElementById("containerTwoImg").src = "img/hort2.png";
-  document.getElementById("figcaption2").innerHTML = "Above: Zooming in, this map highlights the plant beds that make up each horticultural zone. Contact me for PDF of all zones."; 
+  document.getElementById("containerTwoImg").src = "img/other2.png";
+  document.getElementById("figcaption2").innerHTML = ""; 
   document.getElementById("containerThreeImg").style.display = "block";
-  document.getElementById("containerThreeImg").src = "img/hort3.png";
-  document.getElementById("figcaption3").innerHTML = "Above: A snapshot of the most prevalent species on the High Line. The clustering of certain species hints towards the zones' ecosystem."; 
+  document.getElementById("containerThreeImg").src = "img/other3.png";
+  document.getElementById("figcaption3").innerHTML = ""; 
   document.getElementById("containerFourImg").style.display = "block";
-  document.getElementById("containerFourImg").src = "img/hort4.png";
-  document.getElementById("figcaption4").innerHTML = "Above: One way to ascertain shade, light, potential wind conditions, and microclimates on the Park."; 
+  document.getElementById("containerFourImg").src = "img/other1.png";
+  document.getElementById("figcaption4").innerHTML = ""; 
 }
