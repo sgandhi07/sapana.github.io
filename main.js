41084e29d3299c7c4b48851359cbeea79453bb83
sgandhi07 <julie@julies-MacBook-Air.local>
2021-07-01 06:34:50 -0400

    add facilities and design examples to M&O page

--- main.js
+++ main.js
@@ -3,11 +3,24 @@ document.getElementById("design").addEventListener("click", design);
 function design() {
   document.getElementById("containerOneImg").style.display = "block";
   document.getElementById("containerOneImg").src = "img/CAD.png";
-  document.getElementById("figcaption1").innerHTML = "Above: extracting architectural details (benches) from CAD file using ArcMap."; 
-    document.getElementById("containerOneImg").onclick = function() {
-         document.getElementById("containerOneImg").src = "img/CAD.png", 
-         "_blank";
-          modal.style.display = "block";
-          modalImg.src = this.src;
-  }
+  document.getElementById("figcaption1").innerHTML = "Above: Extract architectural details (benches) from CAD file using ArcMap."; 
+  document.getElementById("containerTwoImg").style.display = "block";
+  document.getElementById("containerTwoImg").src = "img/design1.png";
+  document.getElementById("figcaption2").innerHTML = "Above: Design basemap using open data in QGIS"; 
+  document.getElementById("containerThreeImg").style.display = "block";
+  document.getElementById("containerThreeImg").src = "img/design2.png";
+  document.getElementById("figcaption3").innerHTML = "Above: Start to integrate the arhcitectural details with basemap in QGIS"; 
+}
+
+document.getElementById("facilities").addEventListener("click", facilities);
+function facilities() {
+  document.getElementById("containerOneImg").style.display = "block";
+  document.getElementById("containerOneImg").src = "img/facilities1.png";
+  document.getElementById("figcaption1").innerHTML = "Above: Basemap delineating operating zones for maitenance technicians."; 
+  document.getElementById("containerTwoImg").style.display = "block";
+  document.getElementById("containerTwoImg").src = "img/facilities2.png";
+  document.getElementById("figcaption2").innerHTML = "Above: Using work order data entered by maintenance techs, map where unplanned labor hours are spent. <br> We used this map to extropolate which assets required the most attention, and perhaps reassessment of our maintenance strategy."; 
+  document.getElementById("containerThreeImg").style.display = "none";
+  document.getElementById("containerThreeImg").src = "img/facilities3.png";
+  document.getElementById("figcaption3").innerHTML = " "; 
 }
