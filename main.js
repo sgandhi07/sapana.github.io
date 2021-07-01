84e943d26ce91301cb785a1c4d62d973164a2269
sgandhi07 <julie@julies-MacBook-Air.local>
2021-07-01 07:31:58 -0400

    continue filling out the M&O page

--- main.js
+++ main.js
@@ -10,6 +10,8 @@ function design() {
   document.getElementById("containerThreeImg").style.display = "block";
   document.getElementById("containerThreeImg").src = "img/design2.png";
   document.getElementById("figcaption3").innerHTML = "Above: Start to integrate the arhcitectural details with basemap in QGIS"; 
+  document.getElementById("containerFourImg").style.display = "none";
+  document.getElementById("figcaption4").innerHTML = ""; 
 }
 
 document.getElementById("facilities").addEventListener("click", facilities);
@@ -23,4 +25,38 @@ function facilities() {
   document.getElementById("containerThreeImg").style.display = "none";
   document.getElementById("containerThreeImg").src = "img/facilities3.png";
   document.getElementById("figcaption3").innerHTML = " "; 
+  document.getElementById("containerFourImg").style.display = "none";
+  document.getElementById("figcaption4").innerHTML = ""; 
+}
+
+document.getElementById("horticulture").addEventListener("click", horticulture);
+function horticulture() {
+  document.getElementById("containerOneImg").style.display = "block";
+  document.getElementById("containerOneImg").src = "img/hort1.png";
+  document.getElementById("figcaption1").innerHTML = "Above: Basemap delineating horticultural zones for gardeners."; 
+  document.getElementById("containerTwoImg").style.display = "block";
+  document.getElementById("containerTwoImg").src = "img/hort2.png";
+  document.getElementById("figcaption2").innerHTML = "Above: Zooming in, this map highlights the plant beds that make up each horticultural zone. Contact me for PDF of all zones."; 
+  document.getElementById("containerThreeImg").style.display = "block";
+  document.getElementById("containerThreeImg").src = "img/hort3.png";
+  document.getElementById("figcaption3").innerHTML = "Above: A snapshot of the most prevalent species on the High Line. The clustering of certain species hints towards the zones' ecosystem."; 
+  document.getElementById("containerFourImg").style.display = "block";
+  document.getElementById("containerFourImg").src = "img/hort4.png";
+  document.getElementById("figcaption4").innerHTML = "Above: One way to ascertain shade, light, potential wind conditions, and microclimates on the Park."; 
+}
+
+document.getElementById("other").addEventListener("click", other);
+function other() {
+  document.getElementById("containerOneImg").style.display = "block";
+  document.getElementById("containerOneImg").src = "img/hort1.png";
+  document.getElementById("figcaption1").innerHTML = "Above: Basemap delineating horticultural zones for gardeners."; 
+  document.getElementById("containerTwoImg").style.display = "block";
+  document.getElementById("containerTwoImg").src = "img/hort2.png";
+  document.getElementById("figcaption2").innerHTML = "Above: Zooming in, this map highlights the plant beds that make up each horticultural zone. Contact me for PDF of all zones."; 
+  document.getElementById("containerThreeImg").style.display = "block";
+  document.getElementById("containerThreeImg").src = "img/hort3.png";
+  document.getElementById("figcaption3").innerHTML = "Above: A snapshot of the most prevalent species on the High Line. The clustering of certain species hints towards the zones' ecosystem."; 
+  document.getElementById("containerFourImg").style.display = "block";
+  document.getElementById("containerFourImg").src = "img/hort4.png";
+  document.getElementById("figcaption4").innerHTML = "Above: One way to ascertain shade, light, potential wind conditions, and microclimates on the Park."; 
 }
